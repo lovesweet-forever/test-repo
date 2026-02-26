7944e88692bc69b5e606dde906cf6672e4481dad
unknown <Harrison1010>
2026-02-27 01:36:14 +0800

    kevinadriano399@gmail.com

--- src/components/Accordian/index.tsx
+++ src/components/Accordian/index.tsx
@@ -11,8 +11,8 @@ function Accordion(props: IAccordion) {
         return (
             <>
                 <div className="flex accordion_container">
-                    <button id={`${props.id}`} className="flex w-full py-[24px] pl-[16px] pr-[32px] border-b-[1px] border-[solid] border-[#909090]" onClick={props.onclick}>
-                        <span className="text-[16px] text-[#2c2c2c] font-[600]">{props.title}</span>
+                    <button id={`${props.id}`} className="flex w-full py-6 pl-4 pr-8 border-b border-borderprimary" onClick={props.onclick}>
+                        <span className="text-4 text-textsecondary font-semibold">{props.title}</span>
                     </button>
                 </div>
             </>
@@ -21,11 +21,11 @@ function Accordion(props: IAccordion) {
         return (
             <>
                 <div className="flex flex-col accordion_container">
-                    <button id={`${props.id}`} className="flex w-full py-[24px] pl-[16px] pr-[32px] border-l-[2px] border-[solid] border-[#3b63fb]" onClick={props.onclick}>
-                        <span className="text-[16px] text-[#2c2c2c] font-[600]">{props.title}</span>
+                    <button id={`${props.id}`} className="flex w-full py-6 pl-4 pr-8 border-l-2  border-buttonprimary" onClick={props.onclick}>
+                        <span className="text-4 text-textsecondary font-semibold">{props.title}</span>
                     </button>
-                    <div className="flex w-full p-[16px] border-b-[1px] border-[solid] border-[#909090]">
-                        <p className="font-[500] text-[14px] text-[#2c2c2c]">
+                    <div className="flex w-full p-4 border-b  border-borderprimary">
+                        <p className="font-medium text-[14px] text-textsecondary">
                             {props.content}
                         </p>
                     </div>
