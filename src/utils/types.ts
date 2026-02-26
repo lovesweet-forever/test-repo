5ebac1904232f896acb8fa0b12895fa16fc72ff9
Administrator <bluespark0129@gmail.com>
2026-02-27 01:41:14 +0800

    feat/scrum-13

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,3 +5,12 @@ export interface useCardType {
     title: string,
     description: string
 }
+
+export interface adCardType {
+    image: any,
+    width:number,
+    height:number,
+    title: string,
+    description: string
+}
+
