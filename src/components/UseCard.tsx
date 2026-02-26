f806c6ced67d2e478dd7d48b3eee14cf31258776
Administrator <bluespark0129@gmail.com>
2026-02-26 23:36:57 +0800

    modify scrum-12

--- src/components/UseCard.tsx
+++ src/components/UseCard.tsx
@@ -3,13 +3,13 @@ import type { useCardType } from "../utils/types"
 
 const UseCard = (props: useCardType) => {
     return (
-        <div className="h-32.5 w-145.5 grid grid-cols-5">
-            <div className="flex justify-center  items center">
+        <div className="h-32 w-145 grid grid-cols-5 ">
+            <div className="flex justify-center items-center">
                 <ImageComponent name={props.image} width={props.width} height={props.height}/>
             </div>
-            <div className="p-2 col-span-4 border border-bordersecondary content-center">
-                <p className="font-bold text-[18px]">{props.title}</p>
-                <p className="text-[14px]">{props.description}</p>
+            <div className="col-span-4 border p-2 border-bordersecondary content-center">
+                <p className="font-bold text-lg">{props.title}</p>
+                <p className="text-sm">{props.description}</p>
             </div>
         </div>
     )
