26584d99d7ab514642abc795bf775dd2168b701a
Administrator <bluespark0129@gmail.com>
2026-02-27 00:18:32 +0800

    modify UseCase section add title

--- src/pages/UseCases.tsx
+++ src/pages/UseCases.tsx
@@ -4,6 +4,7 @@ import { useCardData } from "../utils/constData";
 const UseCases = () => {
     return (
         <div className="w-full flex flex-column justify-center items-center">
+            <div className="w-200 text-4xl pt-10 pb-10 text-center font-bold">Adobe Commerce use CaseTitle.</div>
             <div className="grid grid-cols-2 gap-3">
                 {
                     useCardData.map((content,index) => 
