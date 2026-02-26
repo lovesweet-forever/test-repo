efbb1f5e53ca41a64d7cc87964332d1a7c7abb01
unknown <Harrison1010>
2026-02-27 07:34:00 +0800

    kevinadriano399@gmail.com

--- src/utils/constData.ts
+++ src/utils/constData.ts
@@ -2,23 +2,29 @@ import ImageNames from "./imageNames"
 
 export const useCardData = [
     {
-        image:ImageNames.bucket,
-        title:"Unify marketing and commerce",
+        image: ImageNames.bucket,
+        title: "Unify marketing and commerce",
         description: "Connect shopping journeys for seamless experiences and higher sales"
     },
     {
-        image:ImageNames.screen,
-        title:"Multi-site scalability",
+        image: ImageNames.screen,
+        title: "Multi-site scalability",
         description: "Scale your storefronts to multiple sites,brand, and markets."
     },
     {
-        image:ImageNames.analysis,
-        title:"B2B self-sevice portal",
+        image: ImageNames.analysis,
+        title: "B2B self-sevice portal",
         description: "Simplify the B2B buying process and unlock new revenue streams."
     },
     {
-        image:ImageNames.home,
-        title:"Streamline commerce operations",
+        image: ImageNames.home,
+        title: "Streamline commerce operations",
         description: "Improve operational efficiency with seamless back-office integration tools."
     }
 ]
+
+export const mainData = {
+    details: "Adobe Commerce",
+    title: "Exceptional commerce experiences.",
+    content: "Adobe Commerce is a composable ecommerce solution that lets you quickly create global, multi-brand B2C and B2B experiences — all from one cloud-native platform. Use it to deliver personalized, high-performance storefront experiences that boost traffic, conversions, and GMV."
+}
