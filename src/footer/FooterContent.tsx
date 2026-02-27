229ae0a157fb672fc1e8daf2b9aa99d696f0fa99
Hong Song <harrison1010@gmail.com>
2026-02-27 16:25:25 +0800

    fix: all fool commits

--- src/footer/FooterContent.tsx
+++ src/footer/FooterContent.tsx
@@ -12,7 +12,7 @@ const FooterContent = () => {
                                 <div className="text-xs flex flex-col grid-col gap-2 ">
                                     {
                                         data.item.map((li, index) =>
-                                            <a key="index" className="text-xs cursor-pointer hover:text-blue-500">{li}</a>
+                                            <a key={index} className="text-xs cursor-pointer hover:text-blue-500">{li}</a>
                                         )
                                     }
                                 </div>
@@ -28,7 +28,7 @@ const FooterContent = () => {
                                 <div className="text-xs flex flex-col grid-col gap-2 ">
                                     {
                                         data.item.map((li, index) =>
-                                            <a key="index" className="text-xs cursor-pointer hover:text-blue-500">{li}</a>
+                                            <a key={index} className="text-xs cursor-pointer hover:text-blue-500">{li}</a>
                                         )
                                     }
                                 </div>
--- src/utils/constData.ts
+++ src/utils/constData.ts
@@ -23,11 +23,6 @@ export const useCardData = [
         description: "Improve operational efficiency with seamless back-office integration tools."
     }
 ]
-export const BreadCrumbData = {
-    home:"Home",
-    products:"Products",
-    adobe:"Adobe Comerce"
-}
 
 export const commerceData:Array<typeCommerce>  = [
   {
@@ -93,28 +88,6 @@ export const commerceData:Array<typeCommerce>  = [
   },
 ]
 
-export const useCardData = [
-    {
-        image: ImageNames.bucket,
-        title: "Unify marketing and commerce",
-        description: "Connect shopping journeys for seamless experiences and higher sales"
-    },
-    {
-        image: ImageNames.screen,
-        title: "Multi-site scalability",
-        description: "Scale your storefronts to multiple sites,brand, and markets."
-    },
-    {
-        image: ImageNames.analysis,
-        title: "B2B self-sevice portal",
-        description: "Simplify the B2B buying process and unlock new revenue streams."
-    },
-    {
-        image: ImageNames.home,
-        title: "Streamline commerce operations",
-        description: "Improve operational efficiency with seamless back-office integration tools."
-    }
-]
 
 export const AccordionData = [
     {
