f7a36fabb1d2bd8dc18e1decdba968d8baa6563b
creditstar <john.jovito990@gmail.com>
2026-02-27 03:41:19 -0800

    feat/scrum-26:AdobeCommerce layout

--- src/utils/constData.ts
+++ src/utils/constData.ts
@@ -22,3 +22,36 @@ export const useCardData = [
         description: "Improve operational efficiency with seamless back-office integration tools."
     }
 ]
+
+export const commerceItemData = [
+    {
+        icon:ImageNames.webniar,
+        title:'Webinar',
+        content:'Adobe Commerce: Building the Experience-Led Commerce Platform'
+    },
+    {
+        icon:ImageNames.caseStudy,
+        title:'Customer',
+        content:`Signify increases net merchandized value by 55%.`
+    },
+    {
+        icon:ImageNames.eventSession,
+        title:'Event session',
+        content:'Adobe Commerce 2025 product roadmap review.'
+    },
+    {
+        icon:ImageNames.guide,
+        title:'Guide',
+        content:'Five must-haves for your B2B and B2C commerce platform.'
+    },
+    {
+        icon:ImageNames.guide,
+        title:'guide',
+        content:'Getting started with Adobe Commerce.'
+    },
+    {
+        icon:ImageNames.eventSession,
+        title:'Event session',
+        content:`HanesBrands creates the world's fastest storefront.`
+    },
+]
