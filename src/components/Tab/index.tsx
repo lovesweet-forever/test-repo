2c60c091f6d7126066414ec9efdc2a3175cbe7dc
creditstar <john.jovito990@gmail.com>
2026-02-26 22:07:56 -0800

    modified feat/scrum-11

--- src/components/Tab/index.tsx
+++ src/components/Tab/index.tsx
@@ -12,7 +12,7 @@ const Tab = (props : propsType) => {
     
     return (
         <div>
-            <div className={`font-bold text-[18px] border-gray ${ props.flg === props.index ? 'text-textsecondary border-x-1 border-t-1 rounded-t-md':'text-textdark border-b-1' }  pt-6 px-8 pb-[18px]`}
+            <div className={`font-bold text-lg border-gray ${ props.flg === props.index ? 'text-textsecondary border-x-1 border-t-1 rounded-t-md':'text-textdark border-b-1' }  pt-6 px-8 pb-4`}
                 onClick={() => props.onClick(props.index)}
             >
                 {props.tabContent.tabItem}
