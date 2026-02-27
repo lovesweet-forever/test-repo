cf97c195aed0fb028e22e6dea7f920ce62014706
unknown <Harrison1010>
2026-02-27 11:21:51 +0800

    kevinadriano399@gmail.com

--- src/utils/constData.ts
+++ src/utils/constData.ts
@@ -2,23 +2,29 @@ import ImageNames from "./imageNames"
 
 export const useCardData = [
     {
-        image:ImageNames.bucket,
-        title:"Unify marketing and commerce",
+        image: ImageNames.bucket,
+        title: "Unify marketing and commerce",
         description: "Connect shopping journeys for seamless experiences and higher sales"
     },
     {
-        image:ImageNames.screen,
-        title:"Multi-site scalability",
+        image: ImageNames.screen,
+        title: "Multi-site scalability",
         description: "Scale your storefronts to multiple sites,brand, and markets."
     },
     {
-        image:ImageNames.analysis,
-        title:"B2B self-sevice portal",
+        image: ImageNames.analysis,
+        title: "B2B self-sevice portal",
         description: "Simplify the B2B buying process and unlock new revenue streams."
     },
     {
-        image:ImageNames.home,
-        title:"Streamline commerce operations",
+        image: ImageNames.home,
+        title: "Streamline commerce operations",
         description: "Improve operational efficiency with seamless back-office integration tools."
     }
 ]
+
+export const learnMoreData = {
+    title: "Adobe commits to agentic commerce standards.",
+    content: "Learn how Adobe Commerce is helping brands capture demand across AI-native shopping channels, while maintaining full control over customer relationships and brand experiences.",
+    explain:["Looking for some of the latest Adobe Commerce releases? Check out ","Adobe Commerce Optimizer"," and ","Adobe Commerce as a Cloud Service"]
+}
