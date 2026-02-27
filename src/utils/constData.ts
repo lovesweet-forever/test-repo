29ce90e3cb70eafc9eed20472bda935883b7e3ec
Administrator <bluespark0129@gmail.com>
2026-02-27 21:49:21 +0800

    feat/scrum-31

--- src/utils/constData.ts
+++ src/utils/constData.ts
@@ -22,3 +22,70 @@ export const useCardData = [
         description: "Improve operational efficiency with seamless back-office integration tools."
     }
 ]
+
+export const contactData = [
+    {
+        topic: 'Contact Us',
+        item: [`Explore all products`]
+    },
+    {
+        topic: 'Our solutions',
+        item: [`Personalization at scale`,
+            `Content supply chain`,
+            `Unified customer experience`,
+            `Creativity and production`,
+            `B2B GTM orchestration`,
+            `Generative AI`
+        ]
+    },
+]
+export const contentsData = [
+    {
+        topic: 'Large & medium business',
+        item: [`Explore all products`,
+            `Acrobat Pro`,
+            `Acrobat Sign`,
+            `Adobe Analytics`,
+            `Adobe Express`,
+            `Adobe Firefly solutions`,
+            `Creative Cloud`,
+            `Experience Manager`,
+            `Frame.io`,
+            `GenStudio for Performance Marketing`,
+            `Real-Time CDP`]
+    },
+    {
+        topic: 'Individuals & small business',
+        item: [`Explore all products`,
+            `Adobe Express`,
+            `Adobe Firefly`,
+            `Adobe Stock`,
+            `Creative Cloud`,
+            `Illustrator`,
+            `Photoshop`,
+            `Adobe Premiere`]
+    },
+    {
+        topic: 'Resources & support',
+        item: [`Resource center`,
+            `Customer success stories`,
+            `Adobe for Business blog`,
+            `Experience Cloud support`,
+            `Creative Cloud support`,
+            `Community forums`,
+            `Development resources`]
+    },
+    {
+        topic: 'Adobe',
+        item: [`About`,
+            `AI Overview`,
+            `Careers`,
+            `Events`,
+            `Newsroom`,
+            `Corporate responsibility`,
+            `Investor relations`,
+            `Supply chain`,
+            `Trust center`,
+            `Adobe for All`]
+    },
+]
