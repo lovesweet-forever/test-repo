9c7f40457936155c719bbbbc5f98abf520091193
creditstar <john.jovito990@gmail.com>
2026-02-27 00:39:23 -0800

    feat/scrum-14:CommerceItem Commponent

--- src/utils/constDatas.ts
+++ src/utils/constDatas.ts
@@ -1,5 +1,5 @@
 import ImageNames from "./imageNames"
-import type { typeCommerce } from "../types/types"
+import type { typeCommerce } from "../utils/types"
 
 export const commerceData:Array<typeCommerce>  = [
   {
